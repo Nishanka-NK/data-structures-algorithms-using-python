2c03877e6cbde73e73d30b89309a714a2a5b5f94
Nishanka <95975715+Nishanka-NK@users.noreply.github.com>
2022-02-14 23:13:05 +0530

    SLL file created

---
+++ singly_linked_list.py
@@ -0,0 +1,51 @@
+# class for node
+
+class node:
+    def __init__(self, datavalue):
+        self.datavalue = datavalue
+        self.next = None
+
+# class for head node
+class head:
+    def __init__(self):
+        self.head = None
+
+
+# instance of node class
+a = node(28)
+b = node(44)
+c = node(3)
+d = node(98)
+
+# instance for head class
+h1 = head()
+
+# declare the head node
+h1.head = a
+
+# declare next nodes
+a.next = b
+b.next = c
+c.next = d
+d.next = None
+
+temp = a
+
+while temp is not None:
+    if temp.next == None:
+      print(temp.datavalue, end=" ---> Null")
+    else:
+        print(temp.datavalue, end=" --->> ")
+
+    temp = temp.next
+
+
+
+
+
+
+
+
+
+
+
